f357af617c0312a7c6a244193cbf8e9c4521a474
SaintOdion <kseon.odion@gmail.com>
2024-02-24 00:48:06 +0200

    task2fix

--- js/task-02.js
+++ js/task-02.js
@@ -1,20 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
-const ingredientsList = document.querySelector('ul#ingredients');
+const ingredientsList = document.querySelector("ul#ingredients");
 
-ingredients.forEach(ingredient => {
-  const listItem = document.createElement('li');
-
+const listItems = ingredients.map((ingredient) => {
+  const listItem = document.createElement("li");
   listItem.textContent = ingredient;
-
-  listItem.classList.add('item');
-
-  ingredientsList.append(listItem);
+  listItem.classList.add("item");
+  return listItem;
 });
+
+ingredientsList.append(...listItems);
